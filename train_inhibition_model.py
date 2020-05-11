a80b381f936f9de29fb15773a95058cefad96b61
zswitten <zswitten@gmail.com>
2020-05-11 12:08:50 -0700

    dataloader

--- train_inhibition_model.py
+++ train_inhibition_model.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
 import random
 
 from inhibition_model import Net
@@ -10,6 +11,33 @@ from make_training_data import make_data
 # train_x, train_y, valid_x, valid_y, test_x, test_y = make_data()
 
 # model = Net(input_dim=train_x.shape[1], hidden_shape=[64, 64])
+
+class InhibitionDataset(Dataset):
+    def __init__(self, df):
+        self.df = df
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.to_list()
+
+        x = self.df.loc[idx, 'gexp'].values
+        x = torch.as_tensor(
+            np.array([row for row in x]),
+            dtype=torch.float32
+        )
+        y = self.df.loc[idx, 'Inh_index'].values
+        y = torch.as_tensor(
+            y, dtype=torch.float32
+        )
+        return {'gexp': x, 'inhibition': y}
+
+def make_data_loader(df, batch_size=32):
+    dataset = InhibitionDataset(df)
+    data_loader = DataLoader(dataset, batch_size=32)
+    return data_loader
 
 def train_epoch(model, x, y, optimizer):
     for x0, y0 in zip(x, y):
